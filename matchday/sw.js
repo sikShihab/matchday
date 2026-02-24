0bb5b148d69fec63718cabfdecce41fc1a7be4bd
Syed Imrul Kayes Shihab <119417929+sikShihab@users.noreply.github.com>
2026-02-24 13:11:29 +0600

    Create sw.js

---
+++ matchday/sw.js
@@ -0,0 +1,3 @@
+self.addEventListener("install", e=>{console.log("Service Worker installed");});
+self.addEventListener("activate", e=>{console.log("Service Worker activated");});
+self.addEventListener("fetch", e=>{ /* caching can be added */ });
